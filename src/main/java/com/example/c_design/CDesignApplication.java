8c15f312ac2a44d4dea751d63ad100e71b7acd33
王一清 <2066813817@qq.com>
2022-09-08 15:23:12 +0800

    第一次提交

---
+++ src/main/java/com/example/c_design/CDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.c_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CDesignApplication.class, args);
+    }
+
+}
