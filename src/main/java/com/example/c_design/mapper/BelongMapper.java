70bef957051b40a5b587082bdda01641c37e95e1
王一清 <2066813817@qq.com>
2022-09-10 13:46:40 +0800

    生成商店从属的持久层代码

---
+++ src/main/java/com/example/c_design/mapper/BelongMapper.java
@@ -0,0 +1,24 @@
+package com.example.c_design.mapper;
+
+import com.example.c_design.domain.Belong;
+import com.example.c_design.domain.BelongExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface BelongMapper {
+    long countByExample(BelongExample example);
+
+    int deleteByExample(BelongExample example);
+
+    int deleteByPrimaryKey(@Param("sId") Integer sId, @Param("uId") Integer uId);
+
+    int insert(Belong record);
+
+    int insertSelective(Belong record);
+
+    List<Belong> selectByExample(BelongExample example);
+
+    int updateByExampleSelective(@Param("record") Belong record, @Param("example") BelongExample example);
+
+    int updateByExample(@Param("record") Belong record, @Param("example") BelongExample example);
+}
